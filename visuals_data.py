79f2b614a320f13f8fe642ee7ccbd33281271165
Peace Amichoh <pamichoh@gmail.com>
2026-03-16 16:55:13 -0500

    called heatmap function to main file and it created headmap html file

--- visuals_data.py
+++ visuals_data.py
@@ -1,3 +1,4 @@
+from folium.plugins import HeatMap
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
@@ -16,9 +17,6 @@ def create_visualizations(df):
     # -------------------------
 
     plt.figure(figsize=(10,6))
-
-    road_counts = df["Highway_Type"].value_counts().head(10).reset_index()
-    road_counts.columns = ["Road_Type","Crash_Count"]
     road_labels = {
         "primary": "Primary Road (Major city road)",
         "secondary": "Secondary Road (Medium traffic)",
@@ -30,11 +28,16 @@ def create_visualizations(df):
         "unclassified": "Minor Road",
         "unknown": "Unknown Road Type"
     }
-    df["Road_Label"] = df["Highway_Type"].map(road_labels)
+
+    df["Road_Label"] = df["Highway_Type"].map(road_labels).fillna("Other Road")
+
+    road_counts = df["Highway_Type"].value_counts().head(10).reset_index()
+    road_counts.columns = ["Road_Type","Crash_Count"]
+
 
     sns.barplot(x="Road_Type", y="Crash_Count", data=road_counts, palette="viridis")
 
-    plt.xticks(rotation=45)
+    plt.xticks(rotation=35, ha = "right")
     plt.title("Top 10 Roads with Most Crashes")
     plt.xlabel("Road Type")
     plt.ylabel("Number of Crashes")
@@ -128,4 +131,25 @@ def create_visualizations(df):
 
     print("Visualizations saved")
 
- 
+def create_crash_heatmap(df):
+
+    print("Creating crash heatmap...")
+
+    lat_mean = df["latitude"].mean()
+    lon_mean = df["longitude"].mean()
+
+    m = folium.Map(location=[lat_mean, lon_mean], zoom_start=11)
+
+    # prepare coordinates
+    heat_data = df[["latitude", "longitude"]].dropna().values.tolist()
+
+    HeatMap(
+        heat_data,
+        radius=10,
+        blur=15,
+        max_zoom=13
+    ).add_to(m)
+
+    m.save("crash_density_heatmap.html")
+
+    print("Heatmap saved") 
